ea0421e7a89389b6d1a967cf08c9c4b931d66087
ashok <kunduruashok5@outlook.com>
2021-11-11 08:54:03 +0530

    new changes

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ namespace GitDemo5
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello its me!");
             Console.WriteLine("Apples");
+            Console.WriteLine("Oranges");
         }
     }
 }
